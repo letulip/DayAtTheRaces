47774e933e19acb688ea44c545b597a0d5a387aa
letulip <ivladimirskiy@ya.ru>
2017-02-28 12:15:29 +0300

    PlayGround is ready

--- DayAtTheRaces/Form1.cs
+++ DayAtTheRaces/Form1.cs
@@ -16,5 +16,43 @@ namespace DayAtTheRaces
         {
             InitializeComponent();
         }
+        
+        private void btnStartRace_Click(object sender, EventArgs e)
+        {
+
+            lblMinimumBet.Text = "All bets are OFF!";
+            btnBets.Hide();
+        }
+
+        private void btnBets_Click(object sender, EventArgs e)
+        {
+            if(lblBettorName.Text == "Joe")
+            {
+                lblJoesBet.Text = numudBet.Value.ToString() + " Bucks to dog No. " + numudDogNumber.Value.ToString();
+            }
+            if (lblBettorName.Text == "Bob")
+            {
+                lblBobsBet.Text = numudBet.Value.ToString() + " Bucks to dog No. " + numudDogNumber.Value.ToString();
+            }
+            if (lblBettorName.Text == "Al")
+            {
+                lblAlsBet.Text = numudBet.Value.ToString() + " Bucks to dog No. " + numudDogNumber.Value.ToString();
+            }
+        }
+
+        private void rbtnJoe_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBettorName.Text = "Joe";
+        }
+
+        private void rbtnBob_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBettorName.Text = "Bob";
+        }
+
+        private void rbtnAl_CheckedChanged(object sender, EventArgs e)
+        {
+            lblBettorName.Text = "Al";
+        }
     }
 }
